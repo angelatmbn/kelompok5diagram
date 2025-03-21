dc73374fe2c06c108e13acca9a6be59a9441d526
haninatasyaa <hanintsyy23@gmail.com>
2025-03-21 15:45:57 +0700

    menyelesaikan modul master data pegawai

---
+++ database/migrations/2025_03_21_081055_create_pegawais_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pegawai', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama');
+            $table->date('tanggal_lahir');
+            $table->text('alamat');
+            $table->string('no_telp');
+            $table->string('shift');
+            $table->timestamps(); 
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pegawai');
+    }
+};
