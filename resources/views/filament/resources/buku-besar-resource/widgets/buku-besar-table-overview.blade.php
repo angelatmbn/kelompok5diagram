39c1121cc822cc160d110b43a39f22918b099300
muhammad ilyas <ilyasiyos15@gmail.com>
2025-06-10 21:16:28 +0700

    Fix bug pada form login

---
+++ resources/views/filament/resources/buku-besar-resource/widgets/buku-besar-table-overview.blade.php
@@ -0,0 +1,6 @@
+{{-- resources/views/filament/resources/buku-besar-resource/widgets/buku-besar-table-overview.blade.php --}}
+
+<div>
+    {{-- Konten HTML/Blade untuk menampilkan data --}}
+    {{-- Akses data dengan $this->getViewData() --}}
+</div>
