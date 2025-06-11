ce5081440c18cf41b82f9b666efdd828ca4da9e4
nadhilatikahs <nadhila.atikahs134@gmail.com>
2025-06-11 17:40:42 +0700

    feat: add pieChart widget on admin dashboard as well

---
+++ app/Filament/Widgets/PenjualanPieChartWidget.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use Filament\Widgets\ChartWidget;
+use Illuminate\Support\Facades\DB;
+
+class PenjualanPieChartWidget extends ChartWidget
+{
+    protected static ?string $heading = 'Penjualan Per Item (Pie Chart) 2025';
+    protected static ?int $sort = 3;
+
+    protected function getData(): array
+    {
+        // Get total quantity sold per menu item
+        $data = DB::table('detail_penjualan')
+            ->join('menu', 'detail_penjualan.menu_id', '=', 'menu.id_menu')
+            ->select('menu.nama_menu', DB::raw('SUM(detail_penjualan.jumlah) as total'))
+            ->groupBy('menu.nama_menu')
+            ->pluck('total', 'menu.nama_menu');
+
+        return [
+            'datasets' => [
+                [
+                    'data' => $data->values(),
+                    'backgroundColor' => [
+                        '#e57373', // color 1
+                        '#64b5f6', // color 2
+                        '#81c784', // color 3
+                        '#ffd54f', // add more if you have more items
+                    ],
+                ],
+            ],
+            'labels' => $data->keys(),
+        ];
+    }
+
+    protected function getType(): string
+    {
+        return 'pie';
+    }
+}
--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -39,6 +39,7 @@ class AdminPanelProvider extends PanelProvider
             ->widgets([
                 Widgets\AccountWidget::class,
                 Widgets\FilamentInfoWidget::class,
+                \App\Filament\Widgets\PenjualanPieChartWidget::class,
             ])
             ->middleware([
                 EncryptCookies::class,
