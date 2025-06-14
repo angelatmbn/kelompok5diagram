94dc34cca10e19cfe52739ddda36317f874aaf9c
AngelaY <angelas@student.telkomuniversity.ac.id>
2025-06-14 16:06:03 +0700

    Perbaikan

--- app/Filament/Resources/JurnalResource.php
+++ app/Filament/Resources/JurnalResource.php
@@ -66,7 +66,7 @@ class JurnalResource extends Resource
                     ->schema([
                         Select::make('coa_id')
                             ->label('Akun')
-                            ->options(Coa::all()->pluck('nama_akun', 'id'))
+                            ->options(coa::all()->pluck('nama_akun', 'id'))
                             ->searchable()
                             ->required(),
                         TextInput::make('debit')
